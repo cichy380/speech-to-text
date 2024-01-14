99a2af1546d8e9aea8a0e1250555d77cf8531ff5
cichy380 <marcin.dobroszek@gmail.com>
2024-01-14 22:15:27 +0100

    restore angular app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AudioRecordingService } from './audio-recording.service';
+import { AudioProcessingService } from './audio-processing.service';
+
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  providers: [AudioProcessingService, AudioRecordingService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+
+  isRecording = false;
+  isConverting = false;
+
+  text = '';
+
+  constructor(
+    private audioRecordingService: AudioRecordingService,
+    private audioProcessingService: AudioProcessingService,
+  ) {
+  }
+
+  startRecording() {
+    this.text = '';
+    this.isRecording = true;
+    this.audioRecordingService.startRecording();
+  }
+
+  async stopRecording() {
+    this.isRecording = false;
+    this.isConverting = true;
+    this.audioRecordingService.stopRecording()
+      .then(audioBlob => {
+        this.audioProcessingService.sendAudio(audioBlob)
+          .subscribe(transcription => {
+            this.isConverting = false;
+            this.text = transcription;
+          });
+      });
+  }
+
 }
