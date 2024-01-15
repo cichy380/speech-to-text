ece394d6d9ed5ecf37c91725ccca73f95843c8b7
cichy380 <marcin.dobroszek@gmail.com>
2024-01-15 21:46:48 +0100

    add Google Cloud Speech credentials in .env

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SpeechClient } from '@google-cloud/speech';
 import { google } from '@google-cloud/speech/build/protos/protos';
 import IRecognizeRequest = google.cloud.speech.v1.IRecognizeRequest;
@@ -9,9 +10,19 @@ export class AppService {
 
   private readonly client: SpeechClient;
 
-  constructor() {
+  constructor(
+      private readonly configService: ConfigService,
+  ) {
     this.client = new SpeechClient({
-      keyFilename: 'speech-to-text-01-411118-01d4c048f21b.json'
+      credentials: {
+        type: configService.get('GOOGLE_CLOUD_SPEECH__CREDENTIALS__TYPE'),
+        client_email: configService.get('GOOGLE_CLOUD_SPEECH__CREDENTIALS__CLIENT_EMAIL'),
+        private_key: Buffer.from(configService.get<string>('GOOGLE_CLOUD_SPEECH__CREDENTIALS__PRIVATE_KEY'), 'base64').toString('utf8'),
+        private_key_id: configService.get('GOOGLE_CLOUD_SPEECH__CREDENTIALS__PRIVATE_KEY_ID'),
+        project_id: configService.get('GOOGLE_CLOUD_SPEECH__CREDENTIALS__PROJECT_ID'),
+        client_id: configService.get('GOOGLE_CLOUD_SPEECH__CREDENTIALS__CLIENT_ID'),
+        universe_domain: configService.get('GOOGLE_CLOUD_SPEECH__CREDENTIALS__UNIVERSE_DOMAIN'),
+      }
     });
   }
 
