f570f2e7744f37d1878a08f510932195794b7205
cichy380 <marcin.dobroszek@gmail.com>
2024-01-16 18:25:41 +0100

    autostop v0.1
    works only one time

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -22,28 +22,39 @@ export class AppComponent {
   text = '';
 
   constructor(
-    private audioRecordingService: AudioRecordingService,
-    private audioProcessingService: AudioProcessingService,
+    private readonly audioRecordingService: AudioRecordingService,
+    private readonly audioProcessingService: AudioProcessingService,
+    private readonly changeDetectorRef: ChangeDetectorRef,
   ) {
   }
 
   startRecording() {
     this.text = '';
     this.isRecording = true;
-    this.audioRecordingService.startRecording();
-  }
-
-  async stopRecording() {
-    this.isRecording = false;
-    this.isConverting = true;
-    this.audioRecordingService.stopRecording()
-      .then(audioBlob => {
-        this.audioProcessingService.sendAudio(audioBlob)
+    this.audioRecordingService.startRecording(audioBlob => {
+      this.isRecording = false;
+      this.isConverting = true;
+      this.changeDetectorRef.detectChanges();
+      this.audioProcessingService.sendAudio(audioBlob)
           .subscribe(transcription => {
             this.isConverting = false;
             this.text = transcription;
+            this.changeDetectorRef.detectChanges();
           });
-      });
+    });
+  }
+
+  async stopRecording() {
+    // this.isRecording = false;
+    // this.isConverting = true;
+    // this.audioRecordingService.stopRecording()
+    //   .then(audioBlob => {
+    //     this.audioProcessingService.sendAudio(audioBlob)
+    //       .subscribe(transcription => {
+    //         this.isConverting = false;
+    //         this.text = transcription;
+    //       });
+    //   });
   }
 
 }
--- frontend/src/app/audio-recording.service.ts
+++ frontend/src/app/audio-recording.service.ts
@@ -1,16 +1,48 @@
 import { Injectable } from '@angular/core';
 
-
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AudioRecordingService {
 
   private mediaRecorder!: MediaRecorder;
+  private audioContext!: AudioContext;
+  private audioAnalyser!: AnalyserNode;
+  private scriptNode!: ScriptProcessorNode | AudioWorkletNode;
   private recordedChunks: Blob[] = [];
-  private stream!: MediaStream;
+  private silenceThreshold = 0.2; // Wartość w decybelach, regulacji według potrzeb
+  private silenceDuration = 1000; // Czas trwania ciszy w milisekundach
+  private silenceStartTime: number | null = null;
 
-  startRecording() {
-    navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
-      this.stream = stream;
+  startRecording(stopRecordingCallback: (audioBlob: Blob) => void) {
+    this.audioContext = new AudioContext();
+    this.audioAnalyser = this.audioContext.createAnalyser();
+    this.scriptNode = this.audioContext.createScriptProcessor(4096, 1, 1);
+
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      const sourceNode = this.audioContext.createMediaStreamSource(stream);
+      sourceNode.connect(this.audioAnalyser);
+      this.audioAnalyser.connect(this.scriptNode);
+      this.scriptNode.connect(this.audioContext.destination);
+
+      // @ts-ignore
+      this.scriptNode.onaudioprocess = () => {
+        const array = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+        this.audioAnalyser.getByteFrequencyData(array);
+
+        const average = this.getAverageVolume(array);
+
+        if (average < this.silenceThreshold) {
+          if (this.silenceStartTime === null) {
+            this.silenceStartTime = Date.now();
+          } else if (Date.now() - this.silenceStartTime > this.silenceDuration) {
+            this.stopRecording().then(audioBlob => stopRecordingCallback(audioBlob));
+          }
+        } else {
+          this.silenceStartTime = null;
+        }
+      };
+
       this.mediaRecorder = new MediaRecorder(stream);
       this.mediaRecorder.start();
       this.mediaRecorder.ondataavailable = (event) => {
@@ -20,16 +52,30 @@ export class AudioRecordingService {
   }
 
   stopRecording() {
-    return new Promise<Blob>((resolve, _reject) => {
+    return new Promise<Blob>((resolve, reject) => {
       this.mediaRecorder.onstop = () => {
         const blob = new Blob(this.recordedChunks, {
           type: 'audio/wav',
         });
         this.recordedChunks = [];
-        this.stream.getTracks().forEach(track => track.stop());
+        this.audioContext.close();
         resolve(blob);
       };
       this.mediaRecorder.stop();
     });
+  }
+
+  private getAverageVolume(array: Uint8Array) {
+    let values = 0;
+    let average;
+
+    const length = array.length;
+
+    for (let i = 0; i < length; i++) {
+      values += array[i];
+    }
+
+    average = values / length;
+    return average;
   }
 }
